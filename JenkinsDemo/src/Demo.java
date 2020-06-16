25f0230b06e97792a8bddbace71c41bc030daa88
Rakshith <rakshithsourav@gmail.com>
2020-06-16 19:22:21 +0530

    version 1.1

--- JenkinsDemo/src/Demo.java
+++ JenkinsDemo/src/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
+		System.out.println("learns jenkins because it makes the developer's job easy");
 	}
 }
