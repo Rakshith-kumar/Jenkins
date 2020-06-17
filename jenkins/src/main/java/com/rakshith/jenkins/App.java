682a0eddcb2dd7ed69005b854218fe8858cc504c
Rakshith <rakshithsourav@gmail.com>
2020-06-17 13:05:09 +0530

    version 1.0 created app class and also defined test cases

---
+++ jenkins/src/main/java/com/rakshith/jenkins/App.java
@@ -0,0 +1,18 @@
+package com.rakshith.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static String test(String name){
+    	return name;
+    }
+}
+
---
+++ jenkins/src/test/java/com/rakshith/jenkins/AppTest.java
@@ -0,0 +1,18 @@
+package com.rakshith.jenkins;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    
+    
+    public AppTest()
+    {
+     assertEquals("Rakshith",App.test("Rakshith"));
+    }
+
+
+}
